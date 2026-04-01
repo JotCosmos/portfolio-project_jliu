ffab9754a8065e32d11bace095ae771a5e7bf1e5
JotCosmos <jliu6798@gmail.com>
2026-04-01 16:03:12 -0400

    Secondary Updates

--- src/animal/AnimalSecondary.java
+++ src/animal/AnimalSecondary.java
@@ -1,10 +1,18 @@
 package animal;
 
+import static org.junit.Assert.assertFalse;
+
+import java.util.Random;
+
 /**
  * Layered implementations of secondary methods for {@code Animal}.
  */
 
 public abstract class AnimalSecondary implements Animal {
+
+    /*
+     * Public members
+     */
 
     /*
      * Common methods (from Object) -------------------------------------------
@@ -24,4 +32,33 @@ public abstract class AnimalSecondary implements Animal {
         boolean result = false;
         return result;
     }
+
+    /*
+     * Other non-kernel methods
+     */
+
+    @Override
+    public String status() {
+        String value = "Species: " + this.species() + "\nName: " + this.name()
+                + "\nGender: " + this.gender() + "\nHealth: " + this.health()
+                + "\nHunger: " + this.hunger() + "\nAlive: " + this.isAlive();
+        return value;
+    }
+
+    @Override
+    public Animal reproduce(Animal other) {
+        assertFalse(this.gender().equals(other.gender()));
+        assertFalse(this.species().equals("Unknown"));
+        assert (this.species().equals(other.species()));
+        Animal child = this.newInstance();
+        Random r = new Random();
+        int randGender = r.nextInt(1);
+        if (randGender == 0) {
+            child.update("gender", "male");
+        } else {
+            child.update("gender", "female");
+        }
+        return child;
+    }
+
 }
